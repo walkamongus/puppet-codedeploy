857937f0bce73abe042aa0c446dd6b70e233b0f8
Chadwick Banning <walkamongus@users.noreply.github.com>
2015-04-30 11:06:40 -0400

    fix region typo

--- manifests/install.pp
+++ manifests/install.pp
@@ -19,7 +19,7 @@ class codedeploy::install {
         ensure => present,
       }
       exec { 'download_codedeploy_installer':
-        command => '/usr/bin/aws s3 cp s3://aws-codedeploy-us-east-1/latest/install . --region is-east-1',
+        command => '/usr/bin/aws s3 cp s3://aws-codedeploy-us-east-1/latest/install . --region us-east-1',
         cwd     => '/home/ubuntu',
         creates => '/home/ubuntu/install'
       }
